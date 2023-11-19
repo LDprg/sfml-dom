e82e614169eeacfbd141a21a10b4ac9e1b945e9e
LDprg <lukas_4dr@gmx.at>
2023-11-19 15:40:25 +0100

    changing color

--- src/hello.cpp
+++ src/hello.cpp
@@ -2,9 +2,13 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+
+    sf::RectangleShape shape2({200, 300});
+    shape2.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
     {
@@ -13,7 +17,11 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            else if (event.type == sf::Event::KeyPressed)
+                if (event.key.code == sf::Keyboard::Space)
+                     shape.setFillColor(shape.getFillColor() == sf::Color::Green ? sf::Color::Red : sf::Color::Green);
         }
+
 
         window.clear();
         window.draw(shape);
